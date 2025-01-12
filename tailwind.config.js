7c5a755d25d13780534df40dae842a1e8f4f69b8
Afif Alfauzan <54790906+MasProxy@users.noreply.github.com>
2025-01-12 16:23:15 +0700

    feat: add a colors variable on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { DEFAULT: "#517C99", hover: "#81AECA" },
+        secondary: { DEFAULT: "#D469C2", hover: "#966BC5" },
+        disabled: { DEFAULT: "#C0BED7", second: "#EFEEFB" },
+        success: { DEFAULT: "#5CBC60", hover: "#85DCA8" },
+        danger: { DEFAULT: "#FD4D4D", hover: "#DC4747" },
+        gray: { DEFAULT: "#DCE1E4", second: "#B0B0B0" },
+        black: { DEFAULT: "#444444" },
+      },
+    },
   },
   plugins: [],
 };
