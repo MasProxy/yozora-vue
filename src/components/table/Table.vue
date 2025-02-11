c98433e4f092ded53fb36131b2c7103e2d68f54c
Afif Alfauzan <54790906+MasProxy@users.noreply.github.com>
2025-02-11 07:35:05 +0700

    feat: create a table component

---
+++ src/components/table/Table.vue
@@ -0,0 +1,32 @@
+<!--
+ * Component: Table
+ * Version: 1.0.0
+ * Author: MasProxy
+ * Date Created: 2024-02-11
+ * Last Modified: 2025-02-11
+ * Description: This is a component for render a table.
+-->
+<script setup lang="ts">
+import { onBeforeMount, Ref, ref } from 'vue'
+
+interface TableProps {
+  fixed?: boolean
+}
+
+const props = withDefaults(defineProps<TableProps>(), {
+  fixed: false,
+})
+
+const finalClass: Ref<string> = ref('')
+
+onBeforeMount(() => {
+  if (props.fixed) {
+    finalClass.value = 'table-fixed'
+  }
+})
+</script>
+<template>
+  <table v-bind="$attrs" :class="finalClass">
+    <slot></slot>
+  </table>
+</template>
