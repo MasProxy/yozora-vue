fbd7bc04be4af7664ffadb64efa9d7c3fefd8f44
Afif Alfauzan <54790906+MasProxy@users.noreply.github.com>
2025-02-11 22:20:45 +0700

    feat: update table data and table header component
    - Add a borderless variant on table data component
    - Add a borderless variant on table header component
    - fix a text and background color on table header component

--- src/components/table/TableHeader.vue
+++ src/components/table/TableHeader.vue
@@ -1,40 +1,45 @@
 <!--
  * Component: Table Header
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: MasProxy
  * Date Created: 2024-02-11
- * Last Modified: 2025-02-11
+ * Last Modified: 2025-02-12
  * Description: This is a component for render a table header.
 -->
 
 <script setup lang="ts">
-import { onBeforeMount, Ref, ref, computed } from 'vue'
+import { onBeforeMount, Ref, ref, computed, CSSProperties } from 'vue'
 
 /**
  * Props for the TableHeader component.
  *
  * @prop {string} width - The width of the table header. Default is ''.
+ * @prop {boolean} borderless - The borderless state of the table header. Default is false.
+ * @prop {string} bgColor - The background color of the table header. Default is 'secondary'.
+ * @prop {string} textColor - The text color of the table header. Default is ''.
+ * @prop {string} textPosition - The text position of the table header. Default is 'center'.
  * @prop {void} onSort - The function to handle sorting. Default is an empty function.
  */
 interface TableHeaderProps {
   width?: string
-  last?: boolean
+  borderless?: boolean
   bgColor?: string
   textColor?: string
+  textPosition?: string
   onSort?: (column: string) => void
 }
 
 const props = withDefaults(defineProps<TableHeaderProps>(), {
   width: '',
   bgColor: 'secondary',
-  last: false,
+  borderless: false,
   textColor: '',
+  textPosition: 'center',
   onSort: () => {},
 })
 
-const finalClass: Ref<string> = ref(
-  'border-l border-t border-b border-gray-700 text-center px-6 py-2'
-)
+const finalClass: Ref<string> = ref('px-4 py-1')
+const finalStyle: Ref<CSSProperties> = ref({})
 
 /**
  * ANCHOR - function to convert color names to hex
@@ -63,30 +68,48 @@ const headerTextColor = computed(() => {
   const b = parseInt(hex.substring(4, 6), 16)
   const yiq = (r * 299 + g * 587 + b * 114) / 1000
 
-  return yiq >= 128 ? 'white' : 'black'
+  return yiq >= 128 ? 'black' : 'white'
 })
 
+/**
+ * ANCHOR - function for generate color
+ */
+const generateColor = () => {
+  const textColor = headerTextColor.value
+
+  finalStyle.value.color = textColor
+  finalStyle.value.background = props.bgColor
+}
+
+/**
+ * ANCHOR - function for generate border
+ */
+const generateBorder = () => {
+  if (props.borderless) {
+    finalClass.value = finalClass.value + ' border-b border-gray-700'
+  } else {
+    finalClass.value =
+      finalClass.value +
+      ' border-l border-t border-gray-700 text-center last:border-r'
+  }
+}
+
 onBeforeMount(() => {
-  if (props.width === '') {
+  if (props.width !== '') {
     finalClass.value = finalClass.value + ` w-[${props.width}]`
   }
 
-  if (props.last) {
-    finalClass.value = finalClass.value + ' border-r'
+  if (props.textPosition !== '') {
+    finalStyle.value.textAlign =
+      (props.textPosition as CSSProperties['textAlign']) ?? 'center'
   }
 
-  if (props.bgColor.includes('#')) {
-    finalClass.value = finalClass.value + ` bg-[${props.bgColor}]`
-  } else {
-    finalClass.value = finalClass.value + ` bg-${props.bgColor}`
-  }
-
-  const textColor = headerTextColor.value
-  finalClass.value = finalClass.value + ` text-${textColor}`
+  generateColor()
+  generateBorder()
 })
 </script>
 <template>
-  <th v-bind="$attrs" :class="finalClass">
+  <th v-bind="$attrs" :class="finalClass" :style="{ ...finalStyle }">
     <slot></slot>
   </th>
 </template>
