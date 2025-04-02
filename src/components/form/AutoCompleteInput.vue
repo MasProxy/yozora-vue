e8b07bc4a1e17bf1e3c10c1f588c57fa6ffe26d3
Afif Alfauzan <54790906+MasProxy@users.noreply.github.com>
2025-04-02 21:13:50 +0700

    feat: add a linked icon on auto complete input component

--- src/components/form/AutoCompleteInput.vue
+++ src/components/form/AutoCompleteInput.vue
@@ -1,9 +1,9 @@
 <!--
  * Component: AutoCompleteInput
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: MasProxy
  * Date Created: 2025-02-23
- * Last Modified: 2025-02-23
+ * Last Modified: 2025-04-02
  * Description: This is a component for autocomplete input field.
 -->
 
@@ -27,6 +27,7 @@ defineOptions({
 
 interface AutoCompleteInputProps {
   isRequesting?: boolean
+  isLinked?: boolean
   modelValue?: string | any
   errorMessage?: string
   size?: string
@@ -37,6 +38,7 @@ interface AutoCompleteInputProps {
 
 const props = withDefaults(defineProps<AutoCompleteInputProps>(), {
   isRequesting: false,
+  isLinked: false,
   modelValue: '',
   errorMessage: '',
   noDataMessage: 'No results found',
@@ -143,12 +145,31 @@ const handleBlur = () => {
       @input="handleInput(($event.target as HTMLInputElement).value)"
       :class="[
         finalClass,
-        'py-2 px-3 border border-gray-300 focus:!ring-primary focus:!outline-primary font-normal rounded-md shadow-sm block w-full disabled:bg-gray-50 disabled:text-gray-500',
+        isLinked || isRequesting ? 'pr-8 pl-3' : 'px-3',
+        'py-2 border border-gray-300 focus:!ring-primary focus:!outline-primary font-normal rounded-md shadow-sm block w-full disabled:bg-gray-50 disabled:text-gray-500',
       ]" />
     <div
       v-if="isRequesting"
       class="absolute inset-y-0 flex items-center right-0 pe-3">
       <Spinner size="5" />
+    </div>
+    <div
+      v-else-if="isLinked"
+      class="absolute inset-y-0 flex items-center right-0 pe-3">
+      <slot>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-link-45deg"
+          viewBox="0 0 16 16">
+          <path
+            d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z" />
+          <path
+            d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z" />
+        </svg>
+      </slot>
     </div>
     <template v-if="errorMessage !== ''">
       <div class="text-xs text-danger font-medium mt-1">
