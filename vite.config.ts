e1a7061c27e5be65427becbc148b48accf49c315
Afif Alfauzan <54790906+MasProxy@users.noreply.github.com>
2026-04-30 07:02:43 +0700

    feat: update tailwind version to 4.x

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
       outDir: 'dist',
       insertTypesEntry: true,
     }),
+    tailwindcss(),
   ],
   build: {
     cssCodeSplit: true,
